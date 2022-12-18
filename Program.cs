49a74c30d0facb2a5641062ea7790ab8c20e16f2
Theo <59589358+TheoSalla@users.noreply.github.com>
2022-12-18 14:34:14 +0100

    added C# server

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GopherServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a listening socket
+            var listener = new TcpListener(IPAddress.Any, 12345);  // listen on port 70
+            listener.Start();
+
+            // accept incoming connections
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                // read the request
+                var request = new byte[1024];
+                int bytesRead = client.GetStream().Read(request, 0, 1024);
+                string requestString = Encoding.ASCII.GetString(request, 0, bytesRead);
+                Console.WriteLine("Received request: " + requestString);
+
+                // construct the response
+                string responseString = "Hello, Gopher!\r\n";
+                byte[] response = Encoding.ASCII.GetBytes(responseString);
+
+                // send the response
+                client.GetStream().Write(response, 0, response.Length);
+
+                // close the connection
+                client.Close();
+            }
+        }
+    }
+}
